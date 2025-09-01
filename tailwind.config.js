48824b81d5435c4f91e13cbcad500f9d473b5608
1v4runboy-sketch <1v4runboy@gmail.com>
2025-08-31 21:34:07 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   darkMode: 'class',
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', "*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: {} },
+  theme: { extend: { colors: { weg: '#0A6CB2' } } },
   plugins: []
 };
