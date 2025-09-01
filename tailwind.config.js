12dd8ae7c493c72842215166f6ccf3d7ad5ecbdc
1v4runboy-sketch <1v4runboy@gmail.com>
2025-08-31 23:20:24 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      // use apenas cores modernas; se precisar do palette oficial:
-      // colors: require('tailwindcss/colors'),
-    },
-  },
-  plugins: [],
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', "*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
+  plugins: []
 };
